101e9df57765bc8f0a3a0b3a98dbc109d316bf73
Ricardo Ogliari <rogliariping@gmail.com>
2021-02-27 14:24:59 -0300

    person.dart

---
+++ lib/sqlite/model/person.dart
@@ -0,0 +1,17 @@
+class Person {
+  int id;
+  final String firstName;
+  final String lastName;
+  final String address;
+
+  Person({this.id, this.firstName, this.lastName, this.address});
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'firstName': firstName,
+      'lastName': lastName,
+      'address': address,
+    };
+  }
+}
