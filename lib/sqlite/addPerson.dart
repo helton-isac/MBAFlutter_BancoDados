98c4bf6cd04ce3d53f5ce6649a46654663940695
Ricardo Ogliari <rogliariping@gmail.com>
2021-02-27 15:06:12 -0300

    estrutura de pastas pro banco nosql

--- lib/sqlite/addPerson.dart
+++ lib/sqlite/addPerson.dart
@@ -15,69 +15,71 @@ class AddPerson extends StatelessWidget {
       appBar: AppBar(
         title: Text("Nova pessoa"),
       ),
-      body: Padding(
-        padding: EdgeInsets.all(16),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              TextFormField(
-                decoration: InputDecoration(
-                    hintText: "Nome",
-                    labelText: "Nome"
-                ),
-                controller: _firstNameController,
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return 'Insira o nome da pessoa';
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                    hintText: "Sobrenome",
-                    labelText: "Sobrenome"
-                ),
-                controller: _lastNameController,
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return 'Insira o sobrenome da pessoa';
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                    hintText: "Endereço",
-                    labelText: "Endereço"
-                ),
-                controller: _addressController,
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return 'Insira o endereço da pessoa';
-                  }
-                  return null;
-                },
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(vertical: 16.0),
-                child: RaisedButton(
-                  onPressed: () {
-                    if (_formKey.currentState.validate()) {
-                      Person person = Person(
-                          firstName: _firstNameController.text,
-                          lastName: _lastNameController.text,
-                          address: _addressController.text
-                      );
-                      Navigator.pop(context, person);
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(16),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                TextFormField(
+                  decoration: InputDecoration(
+                      hintText: "Nome",
+                      labelText: "Nome"
+                  ),
+                  controller: _firstNameController,
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Insira o nome da pessoa';
                     }
+                    return null;
                   },
-                  child: Text('Salvar'),
                 ),
-              ),
-            ],
+                TextFormField(
+                  decoration: InputDecoration(
+                      hintText: "Sobrenome",
+                      labelText: "Sobrenome"
+                  ),
+                  controller: _lastNameController,
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Insira o sobrenome da pessoa';
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                      hintText: "Endereço",
+                      labelText: "Endereço"
+                  ),
+                  controller: _addressController,
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Insira o endereço da pessoa';
+                    }
+                    return null;
+                  },
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 16.0),
+                  child: RaisedButton(
+                    onPressed: () {
+                      if (_formKey.currentState.validate()) {
+                        Person person = Person(
+                            firstName: _firstNameController.text,
+                            lastName: _lastNameController.text,
+                            address: _addressController.text
+                        );
+                        Navigator.pop(context, person);
+                      }
+                    },
+                    child: Text('Salvar'),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
